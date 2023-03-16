cd53e2932153c01286df1e20ecc1d929fe7af5dd
Daniel Pérez <steew0x8@protonmail.com>
2023-03-16 12:47:37 +0100

    finish TCP impl

---
+++ src/control.cpp
@@ -0,0 +1,33 @@
+#include <lwip/sockets.h>  
+
+void setup_tcp() {
+    /* create recv buffer */
+    char rx_buffer[BUFSIZ] = {0};
+
+    /* create socket */
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0) ESP_LOGE("No se pudo crear el socket");
+    int opt = 1;
+    /* set socket options (listen socket) */
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    struct sockaddr_in sa = sockaddr_in {
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_family = AF_INET,
+        .sin_port = htons(6666),
+    };
+    /* bind socket */
+    int err = bind(sock, (struct sockaddr *)&sa, sizeof(sa));
+    if (err != 0) ESP_LOGE("No se pudo vincular el socket");
+    /* start listening in the socket */
+    err = listen(sock, 1);
+
+    for(;;) {
+        int len = 0;
+        do {
+            len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+            rx_buffer[len] = 0;
+            ESP_LOGI(rx_buffer);
+        } while (len > 0);
+    }
+
+}
