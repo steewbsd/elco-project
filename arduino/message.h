cbfa72039cc23d7d45a53b91a37f9786e581ed46
Daniel Pérez <steew0x8@protonmail.com>
2023-04-17 12:04:25 +0200

    fix motor commands to allow backwards movement

--- arduino/message.h
+++ arduino/message.h
@@ -10,13 +10,13 @@ struct Message {
 };
 
 enum CMD_IDENT {
-    MOTOR = 0x00,
-    SENSOR = 0x01,
-    ACK = 0x02,
-    ERR = 0x03
+    MOTOR = 0x01,
+    SENSOR = 0x02,
+    ACK = 0x03,
 };
 
 enum CMD_MOTOR_IDENT {
+    /* These 4 values represent moving the weel forwards */
     // Forward wheel drive (Right)
     FWD_R = 0x01,
     // Forward wheel drive (Left)
@@ -25,9 +25,17 @@ enum CMD_MOTOR_IDENT {
     RWD_R = 0x04,
     // Rear wheel drive (Left)
     RWD_L = 0x08,
-    // Join to make a bitmask
+    /* These 4 values represent moving the weel backwards */
+    // Forward wheel drive (Right) - go backwards
+    FWD_R_BACK = 0x010,
+    // Forward wheel drive (Left) - go backwards
+    FWD_L_BACK = 0x20,
+    // Rear wheel drive (Right) - go backwards
+    RWD_R_BACK = 0x40,
+    // Rear wheel drive (Left) - go backwards
+    RWD_L_BACK = 0x80,
 };
 
-Message msg_gen (CMD_IDENT ident, uint8_t payload);
-uint32_t msg_to_bytes(Message * msg);
+struct Message msg_gen (enum CMD_IDENT ident, uint8_t payload);
+uint32_t msg_to_bytes(struct Message * msg);
 #endif
